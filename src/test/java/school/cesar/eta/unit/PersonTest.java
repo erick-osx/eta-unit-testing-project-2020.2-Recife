42a5b7ae3723f6dbb12dad96f764afc92e1bf034
Milton Castro <miltonmc@gmail.com>
2021-02-06 03:51:50 -0300

    Initial version

---
+++ src/test/java/school/cesar/eta/unit/PersonTest.java
@@ -0,0 +1,62 @@
+package school.cesar.eta.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class PersonTest {
+    @Test
+    public void getName_firstNameJonLastNameSnow_jonSnow() {
+        fail();
+    }
+
+    @Test
+    public void getName_firstNameJonNoLastName_jon() {
+        fail();
+    }
+
+    @Test
+    public void getName_noFirstNameLastNameSnow_snow() {
+        fail();
+    }
+
+    @Test
+    public void getName_noFirstNameNorLastName_throwsException() {
+        fail();
+    }
+
+    @Test
+    public void isBirthdayToday_differentMonthAndDay_false() {
+        fail();
+    }
+
+    @Test
+    public void isBirthdayToday_sameMonthDifferentDay_false() {
+        fail();
+    }
+
+    @Test
+    public void isBirthdayToday_sameMonthAndDay_true() {
+        fail();
+    }
+
+    @Test
+    public void addToFamily_somePerson_familyHasNewMember() {
+        fail();
+    }
+
+    @Test
+    public void addToFamily_somePerson_personAddedAlsoHasItsFamilyUpdated() {
+        fail();
+    }
+
+    @Test
+    public void isFamily_nonRelativePerson_false() {
+        fail();
+    }
+
+    @Test
+    public void isFamily_relativePerson_true() {
+        fail();
+    }
+}
